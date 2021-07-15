7737383cc5ff38a6ac694ec2bd55bcc72443ab9f
GediminasBuda <84792781+GediminasBuda@users.noreply.github.com>
2021-07-15 20:22:07 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture11
+{
+    public class Car
+    {
+        protected string _idNumber;
+        public string _brand;
+
+        public Car(string idNumber, string brand)
+        {
+            _idNumber = idNumber;
+            _brand = brand;
+        }
+        public override string ToString()
+        {
+            return $"{_idNumber} {_brand}";
+        }
+    }
+    public class Dealership
+    {
+        private List<Car> dealership;//static negali buti naudojamas
+
+        public Dealership()
+        {
+            dealership = new List<Car>();
+        }
+
+        public void AddCar(Car car)// cia parametro"car" tipas yra Car!!!!!!!
+        {
+            dealership.Add(car);
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Number of all cars: " + dealership.Count.ToString());//cia be "+" susdedame visa teksta i masyva ar lista;
+            sb.AppendLine("First car in Dealership: " + dealership[0].ToString());
+            sb.AppendLine("Last car in Dealership: " + dealership[dealership.Count - 1].ToString());
+            foreach(var auto in dealership)
+            {
+                sb.AppendLine(auto.ToString());
+            }
+            return sb.ToString();
+        }   
+
+    }
+
+
+}
---
+++ Dealership.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture11
+{ class Program {
+    static void Main(string[] args)
+    {
+        var car1 = new Car("AZA111", "Tesla");
+        var car2 = new Car("AZA555", "Fiat");
+        var car3 = new Car("BBB555", "Bugatti");
+
+        var dealership = new Dealership();
+
+            dealership.AddCar(car1);
+            dealership.AddCar(car2);
+            dealership.AddCar(car3);
+            Console.WriteLine(dealership.ToString());
+    }
+    }
+}
